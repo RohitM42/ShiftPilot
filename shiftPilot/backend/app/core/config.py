6f42a0deaf7cca7046c2ad298e473c6326239c48
RohitM42 <rohitmamtora04@gmail.com>
2026-01-05 17:01:55 +0000

    added gitnore for env file and started setting up database connection

---
+++ shiftPilot/backend/app/core/config.py
@@ -0,0 +1,24 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    # App
+    ENV: str = "development"
+    DEBUG: bool = True
+
+    # Database
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str = "localhost"
+    POSTGRES_PORT: int = 5432
+    POSTGRES_DB: str
+
+    DATABASE_URL: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
+
+
+settings = Settings()
